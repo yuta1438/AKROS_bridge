3d64a494392cffd234767fc3149f282fec15d64c
Hayabusa6863 <o.hayabusa@gmail.com>
2020-10-09 22:14:32 +0900

    cached

--- mbed/AKROS_bridge/NUCLEO_F446RE/basic_op/basic_op.d
+++
@@ -1,6 +0,0 @@
-NUCLEO_F446RE/basic_op/basic_op.o: basic_op/basic_op.cpp \
- /home/fal/gcc4mbed/src/mbed_config.h basic_op/basic_op.h
-
-/home/fal/gcc4mbed/src/mbed_config.h:
-
-basic_op/basic_op.h:
