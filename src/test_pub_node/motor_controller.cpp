c5f4c7e4bc646b821d0476e1b5e2a1fee352540e
Hayabusa6863 <o.hayabusa@gmail.com>
2020-10-29 20:58:15 +0900

    updated

--- src/test_pub_node/motor_controller.cpp
+++ src/test_pub_node/motor_controller.cpp
@@ -1,14 +1,15 @@
+// 複数個モータを搭載
 // controller2: 正弦波状の位置指令を与える．
 
 #include <iostream>
 #include <ros/ros.h>
-#include <AKROS_bridge/motor_cmd_single.h>
-#include <AKROS_bridge/motor_reply_single.h>
-#include <AKROS_bridge/Initialize_single.h>
+#include <AKROS_bridge/motor_cmd.h>
+#include <AKROS_bridge/motor_reply.h>
+#include <AKROS_bridge/Initialize.h>
 
 
 static const double endTime = 15.0; // [s]
-static const double wave_frequency = 1.0;   // [Hz]
+static const double wave_frequency = 0.5;   // [Hz]
 static const double control_frequency = 50.0;  // [Hz]
 static const double amplitude = M_PI / 2;   //[rad]
 
@@ -24,6 +25,8 @@ int main(int argc, char** argv){
     ros::NodeHandle nh;
     ros::Rate loop_rate(control_frequency);
 
+    initialize_client = nh.serviceClient<AKROS_bridge::Initialize_single>("enter_control_mode");
+    finalize_client = nh.serviceClient<AKROS_bridge::Initialize_single>("exit_control_mode");
     cmd_pub = nh.advertise<AKROS_bridge::motor_cmd_single>("motor_cmd", 2);
     
     // 若干のdelayを入れないとCANメッセージが届かない！！！
@@ -32,12 +35,11 @@ int main(int argc, char** argv){
     initialize_client.call(initialize_srv);
     ROS_INFO("Motor Initialized");
     
-    //float q_init = initialize_srv.response.q;
-    float q_init = 0;
+    float q_init = initialize_srv.response.q;
     ROS_INFO("Initial position is %f", q_init);
 
     cmd.id = 0;
-    cmd.Kp = 0.3;
+    cmd.Kp = 0.1;
     cmd.Kd = 0.1;
     cmd.torque = 0;
     //cmd.velocity = 0;
--- src/test_pub_node/single_motor_controller.cpp
+++ src/test_pub_node/single_motor_controller.cpp
@@ -2,6 +2,7 @@
 #include <ros/ros.h>
 #include <eigen3/Eigen/Dense>
 #include <Interpolator.h>
+#include <std_srvs/Empty.h>
 #include <AKROS_bridge/motor_cmd_single.h>
 #include <AKROS_bridge/motor_reply_single.h>
 #include <AKROS_bridge/Initialize_single.h>
@@ -11,12 +12,13 @@ using namespace Eigen;
 using namespace cnoid;
 
 static const double endTime = 3.0;
+static const double extraTime = 5.0;
 static const double control_Hz = 100.0;
 
 ros::Publisher cmd_pub;
-ros::ServiceClient client;
+ros::ServiceClient initialize_client, finalize_client;
 AKROS_bridge::motor_cmd_single cmd;
-AKROS_bridge::Initialize_single srv;
+AKROS_bridge::Initialize_single initialize_srv, finalize_srv;
 Interpolator<Vector2d> interpolator;
 
 
@@ -25,37 +27,38 @@ int main(int argc, char** argv){
     ros::NodeHandle nh;
     ros::Rate loop_rate(control_Hz);
 
-    client = nh.serviceClient<AKROS_bridge::Initialize_single>("enter_control_mode");
-    cmd_pub = nh.advertise<AKROS_bridge::motor_cmd_single>("motor_cmd", 10);
+    initialize_client = nh.serviceClient<AKROS_bridge::Initialize_single>("enter_control_mode");
+    finalize_client = nh.serviceClient<AKROS_bridge::Initialize_single>("exit_control_mode");
+    cmd_pub = nh.advertise<AKROS_bridge::motor_cmd_single>("motor_cmd", 2);
     
     // 若干のdelayを入れないとCANメッセージが届かない！！！
     // 初回だけcan_motor_cmdが二回送信される...
     sleep(0.1);
-    client.call(srv);
+    initialize_client.call(initialize_srv);
     ROS_INFO("Motor Initialized");
     
-    float q_init = srv.response.q;
-    float q_end = q_init + 4*M_PI;
+    float q_init = M_PI_2;
+    float q_end = 0;
 
-    ROS_INFO("Initial position is %f", q_init);
-
+    ROS_INFO("Initial position is %f", initialize_srv.response.q);
+    
     interpolator.clear();
     interpolator.appendSample(0, q_init*Vector2d::Ones());
     interpolator.appendSample(endTime, q_end*Vector2d::Ones());
     interpolator.update();
-
+    
     cmd.id = 0;
     cmd.Kp = 1.0;
-    cmd.Kd = 1.0;
-    //cmd.velocity = 0;
+    cmd.Kd = 0.5;
+    cmd.velocity = 0;
     cmd.torque = 0;
-
+    
     float dq_ref = 0;
     float q_old = 0;
 
-
     Vector2d temp;
     ros::Time t_start = ros::Time::now();
+    
     
     while(ros::ok()){
         double current_time = (ros::Time::now() - t_start).toSec(); // 現在時刻のチェック
@@ -67,20 +70,23 @@ int main(int argc, char** argv){
         // 位置
         cmd.position = q_target;
         // 速度
-        cmd.velocity = (q_target - q_old)/(1.0 / control_Hz);
-        //cmd.velocity = 0;
+        //cmd.velocity = (q_target - q_old)/(1.0 / control_Hz);
+        cmd.velocity = 0;
         // 時間が来たらpublishをやめる
-        if(current_time <= interpolator.domainUpper()){
+        if(current_time <= interpolator.domainUpper() + extraTime){
             cmd_pub.publish(cmd);
             // ROS_INFO("current target: %f");
         }else{
             ROS_INFO("control finished !");
+            finalize_client.call(finalize_srv);
             break;
         }
         q_old = q_target;
         ros::spinOnce();
         loop_rate.sleep();
     }
+
+
 
     return 0;
 }
