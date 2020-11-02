f046c9d82ec02b55f1540bf4861e2b456082755f
Hayabusa6863 <o.hayabusa@gmail.com>
2020-11-03 00:37:56 +0900

    update

--- mbed/slave/slave.cpp
+++ mbed/slave/slave.cpp
@@ -1,12 +1,13 @@
 /* CAN_ROS_Slave_ReceiveOnly */
 // MasterからのCANMessageを受信
 
-// Masterに返信しない！
 // 内容が正しければLEDを点滅 + printfで内容表示
-
+// cmdと同じpos, vel, torを返す．
 
 #include "mbed.h"
 #include "CAN.h"
+
+// F303K8
 #define CAN_RX_PIN  PA_11
 #define CAN_TX_PIN  PA_12
 
@@ -18,30 +19,21 @@
 DigitalOut myled(LED1);
 
 CAN can(CAN_RX_PIN, CAN_TX_PIN);    // CAN通信
-CANMessage Tx_msg, Rx_msg;
 
 Serial pc(USBTX, USBRX, 57600);
+CANMessage Tx_msg;
 
-
-// 受信割込み
-void reply(void){
-    // 指定したIDの受信に成功したら
+void can_Cb(void){
+    CANMessage Rx_msg;
     if(can.read(Rx_msg)){
-        myled = !myled;
-        // IDが合致したら
         if(Rx_msg.id == CAN_SLAVE_ID){
-            // 内容表示
-            for(int i=0; i<Rx_msg.len; i++){
-                pc.printf("%2x,", Rx_msg.data[i]);
-            }
-            pc.printf("\r\n");
-
-            Tx_msg.data[0] = 0x03;
-            Tx_msg.data[1] = 0xFF;
-            Tx_msg.data[2] = 0x00;
-            Tx_msg.data[3] = 0xF0;
-            Tx_msg.data[4] = 0x0F;
-            Tx_msg.data[5] = 0x00;
+            Tx_msg.data[0] = CAN_SLAVE_ID;
+            Tx_msg.data[1] = Rx_msg.data[0];    // Position 8-H
+            Tx_msg.data[2] = Rx_msg.data[1];    // Position 8-L
+            Tx_msg.data[3] = Rx_msg.data[2];    // Velocity 8-H
+            // Velocity 4-L & Torque 4-H
+            Tx_msg.data[4] = (Rx_msg.data[3] & 0xF0) | (Rx_msg.data[6] & 0x0F); 
+            Tx_msg.data[5] = Rx_msg.data[7];
             can.write(Tx_msg);
         }
     }
@@ -53,11 +45,10 @@ int main(void){
     Tx_msg.len = CAN_TX_DATA_LENGTH;
     
     can.frequency(1000000);
-    pc.printf("I am ready !\r\n");
     myled = 0;
     
     // 受信割込み設定(メッセージを受け取ったらreplyを実行)
-    can.attach(&reply);
+    can.attach(&can_Cb);
     
     while(1){
         wait_ms(1);
