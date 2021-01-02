5ece4bc6238c429e03a51acb3c4f2774568b0b3f
Hayabusa6863 <o.hayabusa@gmail.com>
2021-01-02 21:24:30 +0900

    update

--- AKROS_bridge_controller/src/tweak/tweak.cpp
+++ AKROS_bridge_controller/src/tweak/tweak.cpp
@@ -1,5 +1,56 @@
 #include "tweak.h"
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QGridLayout>
 
-tweak_controller::tweak_controller(QWidget* parent) : QDialog(parent){
-    
+
+tweak_controller::tweak_controller(QWidget* parent)
+  : QDialog(parent),
+    nh(){
+    CAN_ID_label = new QLabel("CAN_ID");
+
+    QGridLayout *layout = new QGridLayout;
+    layout->addWidget(enter_tweak_mode_button, 0, 1);
+    layout->addWidget(exit_tweak_mode_button, 0, 2);
+
+
+    tweak_client = nh.serviceClient<AKROS_bridge_msgs::tweak>("tweak");
+    ROS_INFO("Ready for tweak !");
+}
+
+void tweak_controller::tweak_up_Cb(void){
+    // CAN_IDをspinBocから取得
+    //tweak_srv.request.CAN_ID = ;
+    tweak_srv.request.control = 0;
+
+    if(tweak_client.call(tweak_srv)){
+        tweak_srv.response.success = true;
+    }
+}
+
+void tweak_controller::tweak_down_Cb(void){
+    //tweak_srv.request.CAN_ID = ;
+    tweak_srv.request.control = 1;
+
+    if(tweak_client.call(tweak_srv)){
+        tweak_srv.response.success = true;
+    }
+}
+
+void tweak_controller::up_Cb(void){
+    //tweak_srv.request.CAN_ID = ;
+    tweak_srv.request.control = 2;
+
+    if(tweak_client.call(tweak_srv)){
+        tweak_srv.response.success = true;
+    }
+}
+
+void tweak_controller::down_Cb(void){
+    //tweak_srv.request.CAN_ID = ;
+    tweak_srv.request.control = 3;
+
+    if(tweak_client.call(tweak_srv)){
+        tweak_srv.response.success = true;
+    }
 }
--- AKROS_bridge_controller/src/tweak/tweak.h
+++ AKROS_bridge_controller/src/tweak/tweak.h
@@ -16,18 +16,27 @@ class tweak_controller : public QDialog{
 public:
     tweak_controller(QWidget *parent);
 private Q_SLOTS:
+    void tweak_up_Cb();
+    void tweak_down_Cb();
+    void up_Cb();
+    void down_Cb();
+
 
 private:
     ros::NodeHandle nh;
 
-    AKROS_bridge_msgs::tweak srv;
-    ros::ServiceClient tweak_up_client;     // +1
-    ros::ServiceClient tweak_down_client;   // -1
-    ros::ServiceClient up_client;           // +10
-    ros::ServiceClient down_client;         // -10
+    AKROS_bridge_msgs::tweak tweak_srv;
+    ros::ServiceClient tweak_client;
 
     QSpinBox *CAN_ID_spinBox;
     QLabel *CAN_ID_label;
-    void createSpinBox();
+
+    QPushButton* enter_tweak_mode_button;
+    QPushButton* exit_tweak_mode_button;
+    
+    QPushButton* tweak_up_button;
+    QPushButton* tweak_down_button;
+    QPushButton* up_button;
+    QPushButton* down_button;
 
 };
